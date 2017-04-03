8e7fba5b870b9e8187e2ed207b2c7219d0a6e1a1
Valerio Versace <valerio.versace@soluzionifutura.it>
2017-04-03 18:14:33 +0200

    facciamolo un po' meno brutto

--- 09-timeServer.js
+++ 09-timeServer.js
@@ -1,12 +1,14 @@
 'use strict'
 
+const leftPad = (input, charTotal = 2, paddingChar = '0') => input.length < charTotal ? leftPad(paddingChar.concat(input)) : input
+
 require('net')
   .createServer(socket => {
     const date = new Date()
     let month = (date.getMonth() + 1).toString()
-    month = month.length < 2 ? `0${month}` : month
+    month = leftPad(month) 
     let day = date.getDate().toString()
-    day = day.length < 2 ? `0${day}` : day
+    day = leftPad(day) 
     socket.end(`${date.getFullYear()}-${month}-${day} ${date.getHours()}:${date.getMinutes()}\n`)
   })
   .listen(process.argv[2])
