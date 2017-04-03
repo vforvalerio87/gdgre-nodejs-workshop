b5a960507d5bbf384cc30a5bff1682d24f82ee61
Valerio Versace <valerio.versace@soluzionifutura.it>
2017-04-03 18:51:49 +0200

    pulizia

--- 09-timeServer.js
+++ 09-timeServer.js
@@ -5,8 +5,12 @@ const leftPad = (input) => input < 10 ? '0'.concat(input) : input.toString()
 require('net')
   .createServer(socket => {
     const date = new Date()
-    const month = leftPad(date.getMonth() + 1)
-    const day = leftPad(date.getDate())
-    socket.end(`${date.getFullYear()}-${month}-${day} ${date.getHours()}:${date.getMinutes()}\n`)
+    socket.end(
+      `${date.getFullYear()}-` +
+      `${leftPad(date.getMonth() + 1)}-` +
+      `${leftPad(date.getDate())} ` +
+      `${leftPad(date.getHours())}:` +
+      `${leftPad(date.getMinutes())}\n`
+    )
   })
   .listen(process.argv[2])
