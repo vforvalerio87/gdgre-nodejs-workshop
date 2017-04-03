d81433d8ab5734e386fd1992523241265cd56ba2
Valerio Versace <valerio.versace@soluzionifutura.it>
2017-04-03 18:19:30 +0200

    sistemata un'altra cosa schifosetta

--- 05b-modular.js
+++ 05b-modular.js
@@ -1,6 +1,6 @@
 'use strict'
 
-require('./05a-modular')(process.argv[2], process.argv[3], (err, files) => {
+require('./05a-modular')(...process.argv.slice(2, 4), (err, files) => {
   if (err) throw err
   else files.forEach(file => console.log(file))
 })
