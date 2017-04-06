152c7a6deab42a0f8dff763d297c9802f8cd3e10
Valerio Versace <valerio.versace@soluzionifutura.it>
2017-04-06 16:46:30 +0200

    80 col hard limit

--- learnyounode-exercises/12-jsonServer.js
+++ learnyounode-exercises/12-jsonServer.js
@@ -8,10 +8,14 @@ http
     const { pathname, query } = url.parse(req.url, true)
     const date = query.iso ? new Date(query.iso) : NaN
     let body = { 'statusMessage': 'Bad request' }
-    let writeHeadArgs = isNaN(date) ? [400, { 'content-type': 'application/json' }] : [200, { 'content-type': 'application/json' }]
+    let writeHeadArgs = isNaN(date) ?
+      [400, { 'content-type': 'application/json' }] :
+      [200, { 'content-type': 'application/json' }]
     switch (pathname) {
       case '/api/unixtime': {
-        if (!isNaN(date)) { body = Object.assign({}, { 'unixtime': date.getTime() }) }
+        if (!isNaN(date)) {
+          body = Object.assign({}, { 'unixtime': date.getTime() })
+        }
         break
       }
       case '/api/parsetime': {
