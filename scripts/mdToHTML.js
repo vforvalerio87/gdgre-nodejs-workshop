dda5bf927223296df2c3c995066ba8102d5190e5
Valerio Versace <valerio.versace@soluzionifutura.it>
2017-04-06 21:56:06 +0200

    fix script

--- scripts/mdToHTML.js
+++ scripts/mdToHTML.js
@@ -3,22 +3,52 @@
 const fs = require('fs')
 const path = require('path')
 const Transform = require('stream').Transform
-const markdown = require('markdown').markdown
+const showdown = require('showdown')
 
 const appDirectory = fs.realpathSync(process.cwd())
 
-const readStream = fs.createReadStream(path.resolve(
-  appDirectory, 'src', 'corsoNode.md')
-)
-const writeStream = fs.createWriteStream(path.resolve(
+const converter = new showdown.Converter({
+  'noHeaderId': true
+})
+
+const sourceFile = path.resolve(appDirectory, 'src', 'corsoNode.md')
+
+const sourceStream = fs.createReadStream(sourceFile)
+const destStream = fs.createWriteStream(path.resolve(
   appDirectory, 'corsoNode.html')
 )
-const toHTMLStream = new Transform({
+const toHTML = new Transform({
   transform(chunk, _, callback) {
-    callback(null, markdown.toHTML(chunk.toString()))
+    const mdString = chunk.toString('utf8')
+    const html = converter.makeHtml(mdString)
+    callback(null, html)
   }
 })
 
-readStream
-  .pipe(toHTMLStream)
-  .pipe(writeStream)
+const header = [
+  '<!DOCTYPE html>',
+  '<html>',
+  '<head>',
+  '<meta charset="utf-8">',
+  '<meta name="viewport" content="width=device-width, initial-scale=1.0">',
+  '<title>Node.js Workshop - GDG Reggio Emilia</title>',
+  '</head>',
+  '<body>'
+].join('\n')
+
+const footer = [
+  '</body>',
+  '</html>'
+].join('\n')
+
+destStream.write(
+  `${header}\n`,
+  () => {
+    sourceStream
+      .pipe(toHTML)
+      .pipe(destStream, {end: false})
+    sourceStream.on('end', () => {
+      destStream.end(`\n${footer}`)
+    })
+  }
+)
--- src/corsoNode.md
+++ src/corsoNode.md
@@ -2,6 +2,29 @@
 
 ## Scaletta ##
 
-* Argomento 1
-* Argomento 2
-* Argomento 3
+1. Perché una lezione su Node.js
+2. Perché JavaScript 
+3. Argomento 3
+
+
+### 1. Perché una lezione su Node.js
+Node.js è una runtime estremamente popolare basata su JavaScript, linguaggio estremamente popolare.
+
+Popolarità di JavaScript e Node.js:
+* La repository di packages di npm, il package manager di Node.js, è la più grande repository di codice open source del mondo
+* Percentuale di utilizzo di JavaScript per tipologia di developer:<sup><a href="#fn1" id="ref1">[1]</a></sup>
+    * Back-End: 54,5%
+    * Front-End: 90,5%
+    * Full-Stack: 85,3%
+    * Students: 39,1% (2<sup>o</sup> dopo Java)
+    * Mobile: 28,9%
+    * Math & Data: 28,6% 
+
+Perché nasce Node.js e che problema risolve:  
+concurrency tradizionale basata su thread;  
+per la massima parte del tempo, un thread è in attesa di I/O.  
+
+Come Node.js risolve il problema: modello "non-blocking" basato su I/O asincrono con thread singolo e event loop.
+
+## References ##
+1. <a href="#ref1">^</a> <cite id="fn1"><a href="http://stackoverflow.com/insights/survey/2016#most-popular-technologies-per-occupation">"Stack Overflow Developer Survey Results 2016: Most Popular Technologies per Dev Type"</a></cite>
